5e8ab3aeddecca6f3c66223d3d642e1eeb3b8ea6
Surya B <suryaanirudh22@gmail.com>
2025-09-02 11:29:05 +0530

    Create LitmitedPermit.java

---
+++ LitmitedPermit.java
@@ -0,0 +1,29 @@
+import java.util.concurrent.Semaphore;
+
+class ParkingLotResource {
+    private Semaphore parking = new Semaphore(2);
+
+    public void park(int carId) {
+        try{
+            System.out.println("CarId "+carId+" is waiting in queue");
+            parking.acquire();
+            System.out.println("CarId"+carId+"is Parked");
+            Thread.sleep(2000);
+        }catch(Exception e){}
+        finally{
+            System.out.println("CarId "+carId+" leaving the parking lot");
+            parking.release();
+        }
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        ParkingLotResource lot = new ParkingLotResource();
+
+        for (int i = 1; i <= 5; i++) {
+            int carId = i;
+            new Thread(() -> lot.park(carId)).start();
+        }
+    }
+}  1st soln
